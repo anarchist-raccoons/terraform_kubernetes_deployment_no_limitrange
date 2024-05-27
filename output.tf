2d1d87e8a5688229cc43014631bb6d276d3d82f5
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2024-05-27 15:12:43 +0100

    only set load_balancer_ip output if service is a loadBalancer

--- output.tf
+++ output.tf
@@ -8,5 +8,6 @@ output "deployment_resource_version" {
 
 # Display load balancer IP 
 output "load_balancer_ip" {
-  value = kubernetes_service.default.status.0.load_balancer.0.ingress.0.ip
+  #value = kubernetes_service.default.status.0.load_balancer.0.ingress.0.ip
+  value = var.service_type == "LoadBalancer" ? kubernetes_service.default.status.0.load_balancer.0.ingress.0.ip : null
 }
