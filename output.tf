d4da80f45fc453f3fd33728b32313d407b3547e6
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2024-05-27 15:02:49 +0100

    Update output.tf
    later K8 versions take over making public IPs for services so no need to make and reference our own. This change makes sure that the one that k8 makes is referenceable for dns records etc

--- output.tf
+++ output.tf
@@ -5,3 +5,8 @@ output "service_resource_version" {
 output "deployment_resource_version" {
   value = kubernetes_deployment.default.metadata.0.resource_version
 }
+
+# Display load balancer IP 
+output "load_balancer_ip" {
+  value = kubernetes_service.default.status.0.load_balancer.0.ingress.0.ip
+}
