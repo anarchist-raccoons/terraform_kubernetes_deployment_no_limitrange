a724244e05d2f159730a1c069adad9bf476d5076
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2025-03-26 00:05:09 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -5,3 +5,8 @@ output "service_resource_version" {
 output "deployment_resource_version" {
   value = "${kubernetes_deployment.default.metadata.0.resource_version}"
 }
+
+output "load_balancer_ip" {
+  value = kubernetes_service.default.status.0.load_balancer.0.ingress.0.ip
+  #value = var.service_type == "LoadBalancer" ? kubernetes_service.default.status.0.load_balancer.0.ingress.0.ip : null
+}
